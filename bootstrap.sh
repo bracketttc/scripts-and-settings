3c8360b95030d801e3f3a70c8df1151c53000cf2
Timothy Brackett <brackett.tc@gmail.com>
2021-01-06 21:33:14 -0500

    Add bootstrap script to install ansible

---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if ! command -v ansible > /dev/null ; then
+    if ! command -v pip3 > /dev/null ; then
+        package_manager=$(command -v apt-get || command -v dnf || command -v yum)
+        sudo "${package_manager}" install -y python3-pip
+    fi
+    pip3 install ansible
+fi
+
+# install ansible galaxy components
+ansible-galaxy collection install -r requirements.yml
+
+echo "You are now ready to run playbooks in this repository."
